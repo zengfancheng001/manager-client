14addaacca9bae764f69b42b9013903ad289aac4
zengfancheng001 <863513651@qq.com>
2023-12-18 16:37:14 +0800

    登录页面第一次提交

---
+++ src/views/login/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    <h2>login</h2>
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style lang="less" scoped>
+.login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
+</style>
---
+++ src/views/not-found/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="not-found">
+    <h2>你输入的地址不对，请重新输入</h2>
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style lang="less" scoped>
+.not-found {
+  color:purple
+}
+</style>
